ef70ad46ef921fce39b116129f1a9ff5ec63a93e
lyubomirH <126067409+lyubomirH@users.noreply.github.com>
2026-05-15 11:38:11 +0300

    new

---
+++ SMCSB.Data/Configurations/Entities/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMCSB.Data.Configurations.Entities
+{
+    internal class Class1
+    {
+    }
+}
